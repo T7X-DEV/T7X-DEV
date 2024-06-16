805099ccde0a849d43ee4f4b301b347bc66af727
kirbix <kirbix.dev@proton.me>
2024-06-16 13:58:06 +0200

    Create kirbix.py

---
+++ README.py
@@ -0,0 +1,12 @@
+data = {
+  # me ;)
+  "name": "kirbix",
+  # nationalité - 🇫🇷 
+  "nationality": "francais",
+  #💻 languages programmation - langages programming
+  "languages": ["HTML", "CSS", "PYTHON"], 
+  # tools work - outils travaille
+  "framework": "flask",
+  # 🌐network - reseaux 
+ "network": ["https://github.com/kirbix-dev", "https://codepen.io/kirbix", "https://www.reddit.com/user/kirbix-dev"] 
+}
